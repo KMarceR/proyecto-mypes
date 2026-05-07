08cd9586efd0fcedcce302bb63664033636537f7
KMarceR <kmarcebr@gmail.com>
2026-05-06 19:41:00 -0600

    Se creo el Modelo, Factory, Controller Resourse que se utilizaran en la base de datos

---
+++ app/Http/Controllers/PymeController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pyme;
+use Illuminate\Http\Request;
+
+class PymeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Pyme $pyme)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Pyme $pyme)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Pyme $pyme)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Pyme $pyme)
+    {
+        //
+    }
+}
